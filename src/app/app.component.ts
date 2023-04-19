f2e54dd92928248cab50ae8e4a80c1c8fc5ca9bb
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-04-18 19:29:35 -0500

    init layout config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ChatComponent } from '@components/chat';
+import { UserListComponent } from '@components/user-list';
+import { LayoutComponent } from '@core/layout/layout.component';
 
 @Component({
+  standalone: true,
+  imports: [LayoutComponent, UserListComponent, ChatComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-whatsapp';
