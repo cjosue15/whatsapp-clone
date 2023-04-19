225ef30f617541ff2dc632641643faf59bb2f8bc
Josue WSL2 <dev.carlos.morales@gmail.com>
2023-04-19 17:48:23 -0500

    feature(header) finish header on sidebar

--- src/app/core/layout/components/header/header.component.ts
+++ src/app/core/layout/components/header/header.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { ContactPictureComponent } from '@components/contact-picture';
 
 @Component({
   standalone: true,
+  imports: [ContactPictureComponent],
   selector: 'w-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @HostBinding('class.w-header') hostClass = true;
+
   constructor() {}
 
   ngOnInit() {}
